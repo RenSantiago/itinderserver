e00e55535485b00e91decdcda7e87a57cb3ab19b
RenSantiago <RenSantiago@github.com>
2017-11-01 21:43:56 +0800

    chnage server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app     = express();
 // app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 // mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
 // mongoURLLabel = "";
 
